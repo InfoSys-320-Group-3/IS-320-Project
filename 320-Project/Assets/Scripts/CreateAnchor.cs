901ad81c826447bec4da4541f2736541fdc34ee2
Luciano Cassin <luc_cassin@hotmail.com>
2020-10-22 14:12:51 +1300

    Added Remove Anchor Capability
    Will remove first anchor placed

--- 320-Project/Assets/Scripts/CreateAnchor.cs
+++ 320-Project/Assets/Scripts/CreateAnchor.cs
@@ -7,16 +7,26 @@ public class CreateAnchor : MonoBehaviour
     // Variables
     Camera mainCamera;
     public GameObject hazard;
+    static public List<GameObject> spawnedhazards;
 
     void Start()
     {
-        mainCamera = Camera.main;        
+        mainCamera = Camera.main;
+        spawnedhazards = new List<GameObject>();        
     }
 
     public void CreateHazard()
     {
         Vector3 pos = new Vector3(mainCamera.transform.position.x, 1f, mainCamera.transform.position.z);
-        GameObject.Instantiate(hazard, pos, Quaternion.identity);
+        GameObject newHazard = Instantiate(hazard, pos, Quaternion.identity);
+        spawnedhazards.Add(newHazard);
+    }
+
+    public void RemoveHazard()
+    {
+        GameObject toRemove = spawnedhazards[0];
+        spawnedhazards.Remove(toRemove);
+        Destroy(toRemove);
     }
 
 }

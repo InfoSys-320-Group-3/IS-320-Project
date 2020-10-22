c557632377a0469fd0c9abd11ff03e145cbc35fe
Luciano Cassin <luc_cassin@hotmail.com>
2020-10-22 13:36:09 +1300

    Anchor Upgrade
    Upgraded Anchors so they can be placed on position and multiple can be placed

---
+++ 320-Project/Assets/Scripts/CreateAnchor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateAnchor : MonoBehaviour
+{
+    // Variables
+    Camera mainCamera;
+    public GameObject hazard;
+
+    void Start()
+    {
+        mainCamera = Camera.main;        
+    }
+
+    public void CreateHazard()
+    {
+        Vector3 pos = new Vector3(mainCamera.transform.position.x, 1f, mainCamera.transform.position.z);
+        GameObject.Instantiate(hazard, pos, Quaternion.identity);
+    }
+
+}
